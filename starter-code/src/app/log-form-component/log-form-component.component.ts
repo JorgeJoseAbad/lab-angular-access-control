c91cef4e20ef83a3eadffeb53bc3e640f2bdd55d
Jorge Abad <jorgejoseabad@gmail.com>
2017-03-01 21:18:48 +0100

    added getRegister funcionality

--- starter-code/src/app/log-form-component/log-form-component.component.ts
+++ starter-code/src/app/log-form-component/log-form-component.component.ts
@@ -1,28 +1,33 @@
 import { Component, OnInit } from '@angular/core';
- import { LogRegisterService } from '../log-register.service';
+import { LogRegisterService } from '../log-register.service';
 
  @Component({
    selector: 'app-log-form-component',
    templateUrl: './log-form-component.component.html',
    styleUrls: ['./log-form-component.component.css'],
    providers: [LogRegisterService]
- })
- export class LogFormComponentComponent implements OnInit {
+  })
+  export class LogFormComponentComponent implements OnInit {
 
-   constructor(public logRegisterService: LogRegisterService) { }
+     constructor(public logRegisterService: LogRegisterService) { }
 
-   ngOnInit() {
-   }
-
-   submitForm(myForm){
-     console.log(myForm);
-     this.logRegisterService.addRegister(myForm.value.person, myForm.value.message);
-     alert('Added person to log!');
-
-     for (let i = 0; i < this.logRegisterService.logMessages.length; i++){
-       let message = this.logRegisterService.logMessages[i];
-       console.log((i + 1) + ". " + message['person'] + ", " + message['message']+", "+message['createdAt']);
+     ngOnInit() {
      }
-   }
+
+     submitForm(myForm){
+       console.log(myForm);
+       this.logRegisterService.addRegister(myForm.value.person, myForm.value.message);
+       alert('Added person to log!');
+
+       for (let i = 0; i < this.logRegisterService.logMessages.length; i++){
+         let message = this.logRegisterService.logMessages[i];
+         let message2 = this.logRegisterService.getRegisters()[i];
+         console.log(message);
+         console.log(message2);
+         console.log((i + 1) + ". " + message['person'] + ", " + message['message']+", "+message['createdAt']);
+         console.log((i + 1) + ". " + message2['person'] + ", " + message2['message']+", "+message2['createdAt']);
+
+       }
+     }
 
  }
--- starter-code/src/app/log-register.service.ts
+++ starter-code/src/app/log-register.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 
  @Injectable()
  export class LogRegisterService {
-    logMessages: Object[];
-    personToAdd: Object = {};
+      logMessages: Object[];
+      personToAdd: Object = {};
 
-  constructor() {
-     this.logMessages = [];
-   }
+    constructor() {
+       this.logMessages = [];
+     }
 
-   getRegisters(){
-     return this.logMessages;
-  }
+     getRegisters(){
+       return this.logMessages;
+    }
 
-   addRegister(person: string, message: string){
-     this.personToAdd = {person, message, createdAt: new Date()};
-     console.log(this.personToAdd);
-     this.logMessages.push(this.personToAdd);
-   }
+     addRegister(person: string, message: string){
+       this.personToAdd = {person, message, createdAt: new Date()};
+       console.log(this.personToAdd);
+       this.logMessages.push(this.personToAdd);
+     }
 
  }
